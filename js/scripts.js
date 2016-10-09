4a26d0f56b560c58e4b99b6062e2f73914915e23
Vladimir <antonsimeonvladimir@gmail.com>
2016-10-09 20:23:03 +0300

    dfsdfd

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	
+	$('.slider').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'linear',
+	  nextArrow: '<button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Next" role="button" style=""></button>',
+	  prevArrow: '<button type="button" data-role="none" class="slick-prev slick-arrow" aria-label="Previous" role="button" style=""></button>'
+	});
+
+	$('.button-menu').click(function(){
+		$(this).toggleClass('open-nav');
+		if($(this).hasClass('open-nav')) {
+			$('.main-nav').slideDown();
+		}else {
+			$('.main-nav').slideUp();
+		}
+	});
+
+});
